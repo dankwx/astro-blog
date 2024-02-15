82dc6d9c03c0ad14e4fee247c72e1541e2315d1f
Daniel <danielkondlatsch.p@gmail.com>
2024-02-15 16:00:36 +0000

    chore: change localhost port to 8080

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  // change port to 8080 only on amazon ec2
+  server: { port: 8080, host: true}
 });
